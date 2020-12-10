0149d25dc44f217596827d330e10e3fde2fc72bf
unknown <psjishnu13@gmail.com>
2020-12-10 14:44:59 +0530

    made modules

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Welcome to MITS Web'));
+const cookieParser = require('cookie-parser');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//default code of expressjs generator
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//importing routes
+
+const indexRouter = require('./app/modules/index');
+
+app.use('/', indexRouter);
+
 app.listen(PORT, () => console.log(`MITS Web backend running on port ${PORT}`));
