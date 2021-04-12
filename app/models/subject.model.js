63babd5e29ca723f9c64048a077edc9217c52da1
Arihant Hirawat <arihant2310@gmail.com>
2021-04-12 11:22:24 +0530

    adds subject model

---
+++ app/models/subject.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const subjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  //may be theory or lab
+  courseType: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
