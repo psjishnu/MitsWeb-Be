37155ef3186cf93aa85316136d3c3e8c7e10cc3c
Arihant Hirawat <arihant2310@gmail.com>
2021-04-12 21:04:01 +0530

    adds timetable schema

---
+++ app/models/timetable.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const timetableSchema = new mongoose.Schema({
+  department: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  timetable: [
+    {
+      day: String,
+      periodTime: {
+        start: String,
+        end: String,
+      },
+      subject: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("Timetable", timetableSchema);
