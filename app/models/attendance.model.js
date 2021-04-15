c7f397e7129cf1afff377aedcd19aef20ca878cb
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-04-15 16:27:28 +0530

    attendance ✔

---
+++ app/models/attendance.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose").Schema.Types;
+
+const attendanceScheme = new mongoose.Schema({
+  department: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: Number,
+    required: true,
+  },
+  timestamp: { type: String, required: true },
+  /*
+startTime:String,
+endTime:String,
+subjectCode:cs100
+*/
+  period: {
+    type: any,
+    required: true,
+  },
+  attendanceList: { type: any, required: true },
+});
+
+module.exports = mongoose.model("Attendance", attendanceScheme);
