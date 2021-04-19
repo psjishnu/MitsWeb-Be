085eb684e760c2a7cb004b22ec1c8ba2c32ec8e6
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-04-19 17:30:59 +0530

    removed unwanted api �❌❌❌

--- app/routes/default.router.js
+++ app/routes/default.router.js
@@ -7,13 +7,4 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/roll", async (req, res) => {
-  const student = await Student.find({ department: "CSE" });
-  for (var i = 0; i < student.length; i++) {
-    const y = student[i].passoutYear - 4;
-    console.log(student[i]);
-  }
-  res.json({});
-});
-
 module.exports = router;
