52a0b0180b0f80e49d9f02f82cb9ab34957f7acf
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-04-19 17:31:13 +0530

    removed unwanted api �❌❌❌

--- app/routes/default.router.js
+++ app/routes/default.router.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Faculty = require("../models/faculty.model");
-const Student = require("../models/student.model");
 
 router.get("/", (req, res) => {
   res.render("index");
