caeeadb2a99e03b6cf2fcb0086448d4799eed8e5
Arihant Hirawat <arihant2310@gmail.com>
2021-05-15 22:38:53 +0530

    Implement payment processing api

--- app/routes/payment.router.js
+++ app/routes/payment.router.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const { adminAuth } = require("../functions/jwt");
 const { auth } = require("../functions/jwt");
 const PayType = require("../models/paytypes.model");
+const Payment = require("../models/payment.model");
 const { validatePayTypeCreation } = require("./validation/payment.validation");
+const Razorpay = require("razorpay");
+const shortid = require("shortid");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZPORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
 /* 
 ----------------------------Create Payment Type Api's---------------------------------
@@ -39,4 +47,54 @@ router.get("/types", auth, async (req, res) => {
     return res.json({ success: false, msg: err.message });
   }
 });
+
+// to process the payment
+router.post("/razorpay", auth, async (req, res) => {
+  try {
+    const { paymentType, amount } = req.body;
+    const { email } = req.user;
+    const payment_capture = 1;
+    const options = {
+      amount: amount * 100,
+      currency: "INR",
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+    let response = await razorpay.orders.create(options);
+    response = { ...response, paymentType, userEmail: email };
+    return res.json({
+      success: true,
+      data: response,
+    });
+  } catch (err) {
+    console.log(`Failed with error:${err.message}`.red);
+    return res.json({ success: false, msg: err.message });
+  }
+});
+
+router.post("/verification", async (req, res) => {
+  try {
+    const secret = process.env.RAZORPAY_SECRET;
+    const crypto = require("crypto");
+    const shasum = crypto.createHmac("sha256", secret);
+
+    shasum.update(JSON.stringify(req.body));
+
+    const digest = shasum.digest("hex");
+
+    if (digest === req.headers["x-razorpay-signature"]) {
+      const payment = new Payment({
+        payment: req.body,
+      });
+      await payment.save();
+      return res.json({ success: true, status: "ok" });
+    } else {
+      return res.json({ success: false, status: "502" });
+    }
+  } catch (err) {
+    console.log(`Failed with error:${err.message}`.red);
+    return res.json({ success: false, msg: err.message });
+  }
+});
+
 module.exports = router;
