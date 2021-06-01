ebbc22be6bd300e04b9f523bd6af5c289356b822
Arihant Hirawat <arihant2310@gmail.com>
2021-06-01 17:33:07 +0530

    Implement api to get the list of events

--- app/routes/shared.router.js
+++ app/routes/shared.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Subject = require("../models/subject.model");
 const User = require("../models/user.model");
+const Student = require("../models/student.model");
 const ExamType = require("../models/examtype.model");
 const Timetable = require("../models/timetable.model");
 const CourseMaterial = require("../models/coursematerial.model");
@@ -176,18 +177,17 @@ router.post("/event", auth, async (req, res) => {
     const { title, department, semester, start, end, allDay } = req.body;
     const uploadBy = req.user.email;
 
-    const user = await User.findOne({ email: "d" }).select("type");
+    const user = await User.findOne({ email: uploadBy }).select("type");
 
     if (
-      user !== null &&
+      user === null ||
       !["faculty", "admin"].includes(user.type.toLowerCase())
     ) {
-      res.json({
+      return res.json({
         success: false,
         msg: "You are not authorized to perform this action",
       });
     }
-
     const event = new Event({
       title,
       end,
@@ -203,6 +203,44 @@ router.post("/event", auth, async (req, res) => {
     res.json({ success: true, data: event });
   } catch (err) {
     console.log(`Error saving event: ${err.message}`.red);
+    res.json({ success: false, msg: err.message });
+  }
+});
+
+//to get the list of events
+router.get("/event", auth, async (req, res) => {
+  try {
+    const email = req.user.email;
+    const user = await User.findOne({ email });
+
+    if (user !== null) {
+      const type = user.type.toLowerCase();
+      if (type === "admin") {
+        const events = await Event.find({});
+        return res.json({ success: true, data: events });
+      }
+
+      if (type === "faculty") {
+        const events = await Event.find({ uploadBy: email });
+        return res.json({ success: true, data: events });
+      }
+
+      if (type === "student") {
+        const student = await Student.findOne({ email });
+        const { department, currentYear } = student;
+
+        const events = await Event.find({
+          department: { $in: ["all", department] },
+          semester: { $in: [0, currentYear] },
+        });
+
+        return res.json({ success: true, data: events });
+      }
+    }
+
+    res.json({ success: false, msg: "Failed to get events" });
+  } catch (err) {
+    console.log(`Failed to get the events with error: ${err.message}`.red);
     res.json({ success: false, msg: err.message });
   }
 });
