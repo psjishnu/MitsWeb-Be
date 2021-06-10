1400ef1768d316ddb48ab06f964ea9652067f9da
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-06-10 19:03:27 +0530

    fix

--- app/routes/default.router.js
+++ app/routes/default.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.redirect("https://mitsweb.netlify.app/");
+  res.render("index");
 });
 
 module.exports = router;
